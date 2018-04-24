4f34b3195e01ac05b6bdc765f071e6d50b72cab2
chrisperez626 <chrisperez626@gmail.com>
2018-04-23 23:56:20 -0400

    initial program files

---
+++ models/burger.js
@@ -0,0 +1,19 @@
+var orm = require("../config/orm.js");
+
+var Burger = {
+	all: function(cb){
+		orm.selectAll("burgers", function(res){
+			cb(res);
+		})
+	},
+	create: function(colName, values, cb){
+		orm.insertOne("burgers", colName, values, function(res){
+			cb(res);
+		})
+	},
+	eat: function(condition, cb){
+		orm.updateOne("burgers", condition, function(res){
+			cb(res);
+		})
+	}
+}
